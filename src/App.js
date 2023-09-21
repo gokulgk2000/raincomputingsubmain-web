e38ee3d689092a1205a22fe7702bef25eaa2161b
gokul <gokulkrishnan0149@gmail.com>
2023-09-21 11:41:35 +0530

    ui error fix update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import './App.css';
-import { useState } from 'react';
-import { userRegister } from './helpers/backend_helpers';
-import toastr from 'toastr';
-import "toastr/build/toastr.min.css"
-import Header from './components/Navbar';
-import LandingCard from './components/LandingCard';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
+import "./App.css";
+import "toastr/build/toastr.min.css";
+import Header from "./components/Navbar";
+import LandingCard from "./components/LandingCard";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const App = () => {
-
-  
   return (
     <>
-       <Header/>
-    <Routes>
- 
-   <Route path='/' element={ <LandingCard/>} />  
-   
-   
-    </Routes>
-    <Footer/>
-  </>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingCard />} />
+      </Routes>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
